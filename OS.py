d79d75fb47bf95f12ff78562d193f1dba10277d3
Yejin <dpwls8972@gmail.com>
2023-06-04 02:51:39 +0900

    ver1 commit

--- OS.py
+++ OS.py
@@ -1,5 +1,5 @@
 for x in range(2,10): 
     for y in range(1,10):
-        print(x, '*', y, '=', x * y)
+        print(x, "*", y, '=',  x* y, end = '\t')  
         if(y == 9):
-            print('----------')
+            print() # print()로 한칸 띄우기
