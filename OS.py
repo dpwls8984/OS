ceb04dfd67706d121b071100e87749f26a04b610
Yejin <dpwls8972@gmail.com>
2023-06-04 03:23:44 +0900

    ver1 commit

--- OS.py
+++ OS.py
@@ -2,4 +2,4 @@ for x in range(2,10):
     for y in range(1,10):
         print(x, "*", y, '=',  x* y, end = '\t')  
         if(y == 9):
-            print() # print()로 한칸 띄우기
+            print() # print()로 한칸 띄우ㄱㅣ
